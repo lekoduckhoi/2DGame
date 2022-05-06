5a3e0da7cfde8304275ee076864c6b1c21e3ecad
unknown <lekoduckhoi4@gmail.com>
2022-05-06 23:31:40 +0700

    1

---
+++ src/com.main/GamePanel.java
@@ -0,0 +1,28 @@
+
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    
+    //SCREEN SETTINGS
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+
+
+    }
+}
